23117c17ebff3e40c3eb9c2dd49be3757a7e09f7
SynthesisThirtyTwo <154609930+SynthesisThirtyTwo@users.noreply.github.com>
2024-01-10 19:08:25 +0000

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -16,6 +16,11 @@ if ! [[ -f Not+Too+Complicated2-1.5.1-server.zip ]]; then
 	curl -Lo Not+Too+Complicated2-1.5.1-server.zip 'https://mediafilez.forgecdn.net/files/3957/451/Not+Too+Complicated2-1.5.1-server.zip' && unzip -u -o 'Not+Too+Complicated2-1.5.1-server.zip' -d /data
 fi
 
+if ! [[ -f NTC2-TechniParc-Addon.zip ]]; then
+  rm -fr config defaultconfigs scripts forge-*.jar start.sh *Server.zip
+	curl -Lo NTC2-TechniParc-Addon.zip 'https://onedrive.live.com/?redeem=aHR0cHM6Ly8xZHJ2Lm1zL3UvcyFBbjlpd3JaR3pZbUtsUnV0NzJGTjBqRWtNLTZOP2U9aW9VaUEy&cid=8A89CD46B6C2627F&id=8A89CD46B6C2627F%212715&parId=8A89CD46B6C2627F%21454&o=OneUp' && unzip -u -o 'NTC2-TechniParc-Addon.zip' -d /data
+fi
+
 if [[ -n "$OPS" ]]; then
     echo $OPS | awk -v RS=, '{print}' > ops.txt
 fi
